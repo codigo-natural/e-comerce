03a220a8d4b7cf9fcc18a3919913b98d81c5bd08
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-22 12:34:21 -0500

    Introduce new features ✨ add search input

---
+++ src/components/NavIcons/index.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export const NavIcons = () => {
+  return (
+    <div>Nav icons</div>
+  )
+}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import { Menu } from "../Menu";
 import Image from "next/image";
+import { SearchBar } from "../SearchBar/indes";
+import { NavIcons } from "../NavIcons";
 
 type Props = {};
 
 export const Navbar = (props: Props) => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-32 relative">
-      <div className="h-full flex items-center justify-between">
+      <div className="h-full flex items-center justify-between md:hidden">
         {/* MOBILE */}
         <Link href="/">
           <div className="text-2xl tracking-wide">E-comerce</div>
@@ -18,13 +20,14 @@ export const Navbar = (props: Props) => {
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* LEFT */}
         <div className="w-1/3">
-          <Link href="/">
+          <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.png" alt="logo" width={24} height={24} />
             <div className="text-2xl tracking-wide">E-comerce</div>
           </Link>
         </div>
-        <div className="w-2/3">
-        
+        <div className="w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
         </div>
       </div>
     </div>
