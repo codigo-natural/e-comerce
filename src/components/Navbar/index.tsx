32759830e0d19f4a4e18f957c7d7036e22f9e23d
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-22 12:13:43 -0500

    Work in progress 🚧 Responsive Navbar Menu with tailwindcss

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,32 @@
-type Props = {}
+import Link from "next/link";
+import { Menu } from "../Menu";
+import Image from "next/image";
+
+type Props = {};
 
 export const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-32 relative">
+      <div className="h-full flex items-center justify-between">
+        {/* MOBILE */}
+        <Link href="/">
+          <div className="text-2xl tracking-wide">E-comerce</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* DESKTOP */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/* LEFT */}
+        <div className="w-1/3">
+          <Link href="/">
+            <Image src="/logo.png" alt="logo" width={24} height={24} />
+            <div className="text-2xl tracking-wide">E-comerce</div>
+          </Link>
+        </div>
+        <div className="w-2/3">
+        
+        </div>
+      </div>
+    </div>
+  );
+};
