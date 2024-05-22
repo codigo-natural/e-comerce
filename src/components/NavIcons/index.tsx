5a183ca8d43fc6bcfb30b11a3205883e5d4dfd27
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-22 16:19:59 -0500

    feat💄 Responsive Navbar Icons and Modals

--- src/components/NavIcons/index.tsx
+++ src/components/NavIcons/index.tsx
@@ -1,7 +1,65 @@
-'use client'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { CarModal } from "../CartModal";
 
 export const NavIcons = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const router = useRouter();
+
+  // ! Temporary
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen((prev) => !prev);
+  };
+
   return (
-    <div>Nav icons</div>
-  )
-}
+    <div className="flex items-center gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/profile">Profile</Link>
+          <div className="mt-2 cursor-pointer">Logout</div>
+        </div>
+      )}
+      <Image
+        src="/notification.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+
+      <div className="relative cursor-pointer">
+        <Image
+          src="/cart.png"
+          alt=""
+          width={22}
+          height={22}
+          className="cursor-pointer"
+          onClick={() => setIsCartOpen((prev) => !prev)}
+        />
+        <div className="absolute -top-4 -right-4 w-6 bg-lucid rounded-full text-white text-sm flex items-center justify-center">
+          21
+        </div>
+      </div>
+      {isCartOpen && <CarModal />}
+    </div>
+  );
+};
