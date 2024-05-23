9866299cefc6226aca4a0f44dee242f6acc52347
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-23 16:08:37 -0500

    work in progress 🚧 single page design

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,60 @@
+import { Add } from "app/components/Add"
+import { CustomizeProducts } from "app/components/CustomizeProducts"
+import { ProductImages } from "app/components/ProductImages"
+
 const SinglePage = () => {
   return (
-    <div>SinglePage</div>
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        {/* <ProductImages items={product.media?.items} /> */}
+        <ProductImages />
+      </div>
+      {/* TEXTS */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">name product</h1>
+        <p className="text-gray-500">product description</p>
+        <div className="h-[2px] bg-gray-100" />
+          <h2 className="font-medium text-2xl">$ 4645</h2>
+        {/* ) : ( */}
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              $ 4588
+            </h3>
+            <h2 className="font-medium text-2xl">
+              $ 43786
+            </h2>
+          </div>
+        {/* )} */}
+        <div className="h-[2px] bg-gray-100" />
+        {/* {product.variants && product.productOptions ? ( */}
+          <CustomizeProducts
+            // productId={product._id!}
+            // variants={product.variants}
+            // productOptions={product.productOptions}
+          />
+        {/* ) : ( */}
+          {/* <Add
+            productId={product._id!}
+            variantId="00000000-0000-0000-0000-000000000000"
+            stockNumber={product.stock?.quantity || 0}
+          /> */}
+        {/* )} */}
+        <div className="h-[2px] bg-gray-100" />
+        {/* {product.additionalInfoSections?.map((section: any) => ( */}
+          <div className="text-sm" >
+            <h4 className="font-medium mb-4">section title</h4>
+            <p>descriptio selection</p>
+          </div>
+        {/* )) */}
+        <div className="h-[2px] bg-gray-100" />
+        {/* REVIEWS */}
+        <h1 className="text-2xl">User Reviews</h1>
+        {/* <Suspense fallback="Loading...">
+          <Reviews productId={product._id!} />
+        </Suspense> */}
+      </div>
+    </div>
   )
 }
 
--- src/components/NavIcons/index.tsx
+++ src/components/NavIcons/index.tsx
@@ -18,8 +18,9 @@ export const NavIcons = () => {
   const handleProfile = () => {
     if (!isLoggedIn) {
       router.push("/login");
+    } else {
+      setIsProfileOpen((prev) => !prev);
     }
-    setIsProfileOpen((prev) => !prev);
   };
 
   return (
@@ -33,7 +34,7 @@ export const NavIcons = () => {
         onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+        <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href="/profile">Profile</Link>
           <div className="mt-2 cursor-pointer">Logout</div>
         </div>
@@ -46,14 +47,16 @@ export const NavIcons = () => {
         className="cursor-pointer"
       />
 
-      <div className="relative cursor-pointer">
+      <div
+        className="relative cursor-pointer"
+        onClick={() => setIsCartOpen((prev) => !prev)}
+      >
         <Image
           src="/cart.png"
           alt=""
           width={22}
           height={22}
           className="cursor-pointer"
-          onClick={() => setIsCartOpen((prev) => !prev)}
         />
         <div className="absolute -top-4 -right-4 w-6 bg-lucid rounded-full text-white text-sm flex items-center justify-center">
           21
