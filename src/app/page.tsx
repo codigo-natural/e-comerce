926372f76a0f9e5d182baf16056648fc39ee6743
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-23 07:33:49 -0500

    feat 🎉 sliding category list design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CategoryList } from "app/components/CategoryList";
 import { ProductList } from "app/components/ProductList";
 import { Slider } from "app/components/Slider/indes";
 
@@ -9,6 +10,14 @@ export default function Home() {
         <h1 className="text-2xl">Featured Product</h1>
         <ProductList />
       </div>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">Categories</h1>
+        <CategoryList />
+      </div>
+      {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <ProductList />
+      </div> */}
     </main>
   );
 }
