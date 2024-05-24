88c43d8acedc6642e3963b856aebda90a6bb8c34
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-23 22:27:31 -0500

    feat 🚀 fetch products from wix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+// "use client";
+
 import { CategoryList } from "app/components/CategoryList";
 import { ProductList } from "app/components/ProductList";
 import { Slider } from "app/components/Slider/indes";
+import { WixClientContext } from "app/context/wixContext";
+import { useWixClient } from "app/hooks/useWixClient";
+import { wixClientServer } from "app/lib/wixClientServer";
+import { useEffect } from "react";
 
-export default function Home() {
+export default async function Home () {
+  // const wixClient = useWixClient();
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+  //     console.log(res);
+  //   };
+
+  //   getProducts();
+  // }, [wixClient]);
+
+  const wixClient = await wixClientServer()
+
+  const res = await wixClient.products.queryProducts().find();
+  console.log(res)
+
   return (
     <main className="">
       <Slider />
@@ -11,7 +33,9 @@ export default function Home() {
         <ProductList />
       </div>
       <div className="mt-24">
-        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">Categories</h1>
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">
+          Categories
+        </h1>
         <CategoryList />
       </div>
       {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
