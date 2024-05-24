ca84471a3b545cfa7ea1b4f6e4e1a2956910949e
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-24 12:26:59 -0500

    feat ✨ fetch a single product

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,61 +1,78 @@
-import { Add } from "app/components/Add"
-import { CustomizeProducts } from "app/components/CustomizeProducts"
-import { ProductImages } from "app/components/ProductImages"
+import { Add } from "app/components/Add";
+import { CustomizeProducts } from "app/components/CustomizeProducts";
+import { ProductImages } from "app/components/ProductImages";
+import { wixClientServer } from "app/lib/wixClientServer";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
-const SinglePage = () => {
-  return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
-      {/* IMG */}
-      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        {/* <ProductImages items={product.media?.items} /> */}
-        <ProductImages />
-      </div>
-      {/* TEXTS */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">name product</h1>
-        <p className="text-gray-500">product description</p>
-        <div className="h-[2px] bg-gray-100" />
-          <h2 className="font-medium text-2xl">$ 4645</h2>
-        {/* ) : ( */}
-          <div className="flex items-center gap-4">
-            <h3 className="text-xl text-gray-500 line-through">
-              $ 4588
-            </h3>
-            <h2 className="font-medium text-2xl">
-              $ 43786
-            </h2>
-          </div>
-        {/* )} */}
-        <div className="h-[2px] bg-gray-100" />
-        {/* {product.variants && product.productOptions ? ( */}
-          <CustomizeProducts
-            // productId={product._id!}
-            // variants={product.variants}
-            // productOptions={product.productOptions}
-          />
-        {/* ) : ( */}
-          {/* <Add
-            productId={product._id!}
-            variantId="00000000-0000-0000-0000-000000000000"
-            stockNumber={product.stock?.quantity || 0}
-          /> */}
-        {/* )} */}
-        <div className="h-[2px] bg-gray-100" />
-        {/* {product.additionalInfoSections?.map((section: any) => ( */}
-          <div className="text-sm" >
-            <h4 className="font-medium mb-4">section title</h4>
-            <p>descriptio selection</p>
-          </div>
-        {/* )) */}
-        <div className="h-[2px] bg-gray-100" />
-        {/* REVIEWS */}
-        <h1 className="text-2xl">User Reviews</h1>
-        {/* <Suspense fallback="Loading...">
-          <Reviews productId={product._id!} />
-        </Suspense> */}
-      </div>
-    </div>
-  )
-}
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  console.log(params.slug);
+  const wixClient = await wixClientServer();
 
-export default SinglePage
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+    if (!products.items[0]) {
+      return notFound()
+    }
+
+    const product = products.items[0]
+
+    return (
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+        {/* IMG */}
+        <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+          <ProductImages items={product.media?.items} />
+        </div>
+        {/* TEXTS */}
+        <div className="w-full lg:w-1/2 flex flex-col gap-6">
+          <h1 className="text-4xl font-medium">{product.name}</h1>
+          <p className="text-gray-500">{product.description}</p>
+          <div className="h-[2px] bg-gray-100" />
+          {product.price?.price === product.price?.discountedPrice ? (
+            <h2 className="font-medium text-2xl">${product.price?.price}</h2>
+          ) : (
+            <div className="flex items-center gap-4">
+              <h3 className="text-xl text-gray-500 line-through">
+                ${product.price?.price}
+              </h3>
+              <h2 className="font-medium text-2xl">
+                ${product.price?.discountedPrice}
+              </h2>
+            </div>
+          )}
+          <div className="h-[2px] bg-gray-100" />
+          {product.variants && product.productOptions ? (
+            <CustomizeProducts
+              // productId={product._id!}
+              // variants={product.variants}
+              // productOptions={product.productOptions}
+            />
+          ) : (
+            <Add
+              // productId={product._id!}
+              // variantId="00000000-0000-0000-0000-000000000000"
+              // stockNumber={product.stock?.quantity || 0}
+            />
+          )}
+          <div className="h-[2px] bg-gray-100" />
+          {product.additionalInfoSections?.map((section: any) => (
+            <div className="text-sm" key={section.title}>
+              <h4 className="font-medium mb-4">{section.title}</h4>
+              <p>{section.description}</p>
+            </div>
+          ))}
+          <div className="h-[2px] bg-gray-100" />
+          {/* REVIEWS */}
+          <h1 className="text-2xl">User Reviews</h1>
+          {/* <Suspense fallback="Loading...">
+            <Reviews productId={product._id!} />
+          </Suspense> */}
+        </div>
+      </div>
+    );
+};
+
+export default SinglePage;
