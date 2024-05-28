0738d4b0c4a4a7ac651ea31256b4c26df6254a2c
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-28 08:37:59 -0500

    feat ✨ add loader skeletons

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,6 @@
 import { Filter } from "app/components/Filter";
 import { ProductList } from "app/components/ProductList";
+import { Skeleton } from "app/components/Skeleton";
 import { wixClientServer } from "app/lib/wixClientServer";
 import Image from "next/image";
 import { Suspense } from "react";
@@ -29,7 +30,7 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
       <Filter />
       {/* PRODUCTS */}
       <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
-      <Suspense fallback={"loading..."}>
+      <Suspense fallback={<Skeleton />}>
         <ProductList
           categoryId={
             cat.collection?._id || "00000000-000000-000000-000000000001"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 
 import { CategoryList } from "app/components/CategoryList";
 import { ProductList } from "app/components/ProductList";
+import { Skeleton } from "app/components/Skeleton";
 import { Slider } from "app/components/Slider/indes";
-import { WixClientContext } from "app/context/wixContext";
-import { useWixClient } from "app/hooks/useWixClient";
-import { wixClientServer } from "app/lib/wixClientServer";
+// import { WixClientContext } from "app/context/wixContext";
+// import { useWixClient } from "app/hooks/useWixClient";
+// import { wixClientServer } from "app/lib/wixClientServer";
 import { Suspense, useEffect } from "react";
 
 export default async function Home() {
@@ -30,7 +31,7 @@ export default async function Home() {
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Product</h1>
-        <Suspense fallback={"loading"}>
+        <Suspense fallback={<Skeleton />}>
           <ProductList
             categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
             limit={4}
@@ -41,14 +42,19 @@ export default async function Home() {
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">
           Categories
         </h1>
-        <Suspense fallback={"loading"}>
+        <Suspense fallback={<Skeleton />}>
           <CategoryList />
         </Suspense>
       </div>
-      {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList />
-      </div> */}
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
+      </div>
     </main>
   );
 }
